2a7941129abbc5091343e543ad20f6f11fb6c533
xyz-33 <62939458+xyz-33@users.noreply.github.com>
2023-09-24 12:08:26 -0700

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5100/api',
+        target: 'https://job-track-app-backend.onrender.com:5100/api',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
